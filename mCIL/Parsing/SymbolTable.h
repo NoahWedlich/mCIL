4b9f30985d4adea40cc00eb7c665b93226bde6ff
Noah Wedlich <43807121+NoahWedlich@users.noreply.github.com>
2023-05-25 13:53:38 +0200

    Added a SymbolTable

---
+++ mCIL/Parsing/SymbolTable.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "../cil-system.h"
+#include "../Types/Type.h"
+#include "../Types/TypeTable.h"
+#include "../Types/cil-types.h"
+
+class SymbolTable
+{
+public:
+	//TODO: Add definitions and the respective code-blocks
+	SymbolTable();
+
+	void decl_loc_variable(VarInfo info);
+	void decl_loc_function(FuncInfo info);
+	void decl_loc_class(ClassInfo info);
+
+	static void decl_glob_variable(VarInfo info);
+	static void decl_glob_function(FuncInfo info);
+	static void decl_glob_class(ClassInfo info);
+
+	const bool exists_loc_variable(std::string name) const;
+	const bool exists_loc_function(std::string name) const;
+	const bool exists_loc_class(std::string name) const;
+
+	static const bool exists_glob_variable(std::string name);
+	static const bool exists_glob_function(std::string name);
+	static const bool exists_glob_class(std::string name);
+
+	const VarInfo get_loc_variable(std::string name) const;
+	const FuncInfo get_loc_function(std::string name) const;
+	const ClassInfo get_loc_class(std::string name) const;
+
+	static const VarInfo get_glob_variable(std::string name);
+	static const FuncInfo get_glob_function(std::string name);
+	static const ClassInfo get_glob_class(std::string name);
+
+	static void make_table_global(SymbolTable& table);
+private:
+	std::unordered_map<std::string, VarInfo> vars_;
+	std::unordered_map<std::string, FuncInfo> funcs_;
+	std::unordered_map<std::string, ClassInfo> classes_;
+
+	static SymbolTable global_table_;
+};
